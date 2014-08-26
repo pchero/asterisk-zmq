93b64628ba28e500974d716c5f1a7efc8aaf4e89
Sungtae Kim <stk@ipvision.dk	>
2014-08-26 11:24:50 +0200

    Source commit.
    Note:
    - First source commit.

---
+++ Makefile
@@ -0,0 +1,77 @@
+
+
+#### Compiler and tool definitions shared by all build targets #####
+CC = gcc
+UNAME := $(shell uname)
+# -Wall -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations
+BASICOPTS = -Wall -g -pthread -pipe -g3 -O6 -fPIC -DAST_MODULE=\"res_zmq_manager\"
+CFLAGS = $(BASICOPTS)
+
+# Define the target directories.
+TARGETDIR_res_zmq_manager.so=build
+
+
+all: $(TARGETDIR_res_zmq_manager.so)/res_zmq_manager.so
+
+## Target: res_zmq_manager.so
+CFLAGS_res_zmq_manager.so = \
+	-I/usr/include/ \
+	-I/usr/local/include/ \
+	-I/opt/asterisk/include/
+CPPFLAGS_res_zmq_manager.so = 
+OBJS_res_zmq_manager.so =  \
+	$(TARGETDIR_res_zmq_manager.so)/res_zmq_manager.o
+
+# WARNING: do not run this directly, it should be run by the master Makefile
+SHAREDLIB_FLAGS_res_zmq_manager.so = 
+
+PKGCONFIG="pkg-config"
+OSLDLIBS=
+
+ifeq ($(UNAME), Linux)
+	SHAREDLIB_FLAGS_res_zmq_manager.so = -shared -Xlinker -x -Wl,--hash-style=gnu -Wl,--as-needed -rdynamic
+endif
+
+ifeq ($(UNAME), Darwin)
+	PKGCONFIG=$(shell if [ "x$(HOMEBREW_PREFIX)" == "x" ];then echo "/usr/local/bin/pkg-config"; else echo "$(HOMEBREW_PREFIX)/bin/pkg-config"; fi)
+
+	# Link or archive
+	SHAREDLIB_FLAGS_res_zmq_manager.so = -bundle -Xlinker -macosx_version_min -Xlinker 10.4 -Xlinker -undefined -Xlinker dynamic_lookup -force_flat_namespace
+	OSLDLIBS=/usr/lib/bundle1.o
+endif
+
+JANSSON_DEFS=$(shell $(PKGCONFIG) jansson --cflags 2>/dev/null)
+JANSSON_LIB=$(shell $(PKGCONFIG)  jansson --libs  2>/dev/null)
+
+ZMQ_DEFS=$(shell $(PKGCONFIG) libzmq --cflags 2>/dev/null)
+ZMQ_LIB=$(shell $(PKGCONFIG)  libzmq --libs  2>/dev/null)
+
+CFLAGS_res_zmq_manager.so += $(ZMQ_DEFS) $(JANSSON_DEFS)
+LDLIBS_res_zmq_manager.so = $(OSLDLIBS) $(ZMQ_LIB) $(JANSSON_LIB)
+
+$(TARGETDIR_res_zmq_manager.so)/res_zmq_manager.so: $(TARGETDIR_res_zmq_manager.so) $(OBJS_res_zmq_manager.so) $(DEPLIBS_res_zmq_manager.so)
+	$(LINK.c) $(CFLAGS_res_zmq_manager.so) $(CPPFLAGS_res_zmq_manager.so) -o $@ $(OBJS_res_zmq_manager.so) $(SHAREDLIB_FLAGS_res_zmq_manager.so) $(LDLIBS_res_zmq_manager.so)
+
+
+# Compile source files into .o files
+$(TARGETDIR_res_zmq_manager.so)/res_zmq_manager.o: $(TARGETDIR_res_zmq_manager.so) src/res_zmq_manager.c
+	$(COMPILE.c) $(CFLAGS_res_zmq_manager.so) $(CPPFLAGS_res_zmq_manager.so) -o $@ src/res_zmq_manager.c
+
+
+
+#### Clean target deletes all generated files ####
+clean:
+	rm -f \
+		$(TARGETDIR_res_zmq_manager.so)/res_zmq_manager.so \
+		$(TARGETDIR_res_zmq_manager.so)/res_zmq_manager.o
+	rm -f -r $(TARGETDIR_res_zmq_manager.so)
+
+
+# Create the target directory (if needed)
+$(TARGETDIR_res_zmq_manager.so):
+	mkdir -p $(TARGETDIR_res_zmq_manager.so)
+
+
+# Enable dependency checking
+#.KEEP_STATE:
+#.KEEP_STATE_FILE:.make.state.GNU-amd64-Linux
---
+++ src/res_zmq_manager.c
@@ -0,0 +1,583 @@
+/*!
+  \file   res_zmq_manager.c
+  \brief
+
+  \author Sungtae Kim
+  \date   Aug 22, 2014
+
+ */
+
+#define _GNU_SOURCE
+
+#include <signal.h>
+#include <stdbool.h>
+
+#include <zmq.h>
+#include <jansson.h>
+
+#include <asterisk.h>
+#include <asterisk/module.h>
+#include <asterisk/cli.h>
+#include <asterisk/utils.h>
+#include <asterisk/manager.h>
+#include <asterisk/config.h>
+#include <asterisk/channel.h>
+#include <asterisk/ast_version.h>
+
+ASTERISK_FILE_VERSION(__FILE__, "$Revision: 338557 $")
+
+#define DEBUG(fmt, args...) ast_log(AST_LOG_VERBOSE, "[0MQ Manager Debug]: "fmt, args);
+#define ERROR(fmt, args...) ast_log(LOG_ERROR, "[0MQ Manager Error]: "fmt, args);
+#define MAX_RCV_BUF_LEN 8192
+
+struct unload_string {
+    AST_LIST_ENTRY(unload_string) entry;
+    struct ast_str *str;
+};
+
+struct ast_zmq_pthread_data {
+    pthread_t master;
+    int accept_fd;
+    void *(*fn)(void *);
+    const char *name;
+};
+
+typedef struct zmq_data_t_
+{
+    void* zmq_sock;     //!< zmq socket
+    json_t* j_recv;     //!< zmq recv data
+} zmq_data_t;
+
+struct app_
+{
+    pthread_t       pth_cmd;      //!< cmd process thread.
+    pthread_t       pth_evt;      //!< evt process thread.
+    struct ast_str* addr_cmd;     //!< cmd socket address
+    struct ast_str* addr_evt;     //!< evt socket address
+
+    char* config_name;
+    void* sock_ctx; //!< zmq context
+    void* sock_cmd; //!< zmq command socket.
+    void* sock_evt; //!< zmq event socket.
+};
+
+static void zmq_cmd_thread(void);
+static void trim(char * s);
+static void zmq_cmd_handler(void *data);
+static int ast_zmq_start(void);
+static char* handle_cli_zmq_manager_status(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a);
+static int zmq_evt_helper(int category, const char *event, char *content);
+
+static struct ast_cli_entry cli_zmq_manager_evt[] = {
+    AST_CLI_DEFINE(handle_cli_zmq_manager_status, "Shows useful status about zmq manager"),
+};
+
+
+static json_t*  g_json_res;  //!< action cmd response
+static json_t*  g_json_evt;     //!< Event notify
+
+//static ast_mutex_t workers_mutex;
+static AST_LIST_HEAD_STATIC(unload_strings, unload_string);
+
+static struct app_ g_app;
+
+
+/* The helper function is required by struct manager_custom_hook. See __manager_event for details */
+static int amihook_helper(int category, const char *event, char *content)
+{
+    ast_log(LOG_NOTICE, "AMI Event: category[%d], event[%s], content[%s]\n", category, event, content);
+    return 0;
+}
+
+/* The helper function is required by struct manager_custom_hook. See __manager_event for details */
+/**
+ *
+ * @param category
+ * @param event
+ * @param content
+ * @return
+ */
+static int zmq_cmd_helper(int category, const char *event, char *content)
+{
+    char* key;
+    char* value;
+
+    ast_log(LOG_NOTICE, "AMI Event: category[%d], event[%s], content[%s]\n", category, event, content);
+    key = strtok(content, ":");
+    value = strtok(NULL, "\r");
+    if(key == NULL)
+    {
+        return false;
+    }
+
+    json_object_set(g_json_res, key, json_string(value));
+    return 0;
+}
+
+/**
+ *
+ * @param cfg
+ * @param category
+ * @param variable
+ * @param field
+ * @param def
+ * @return
+ */
+static int load_config_string(struct ast_config *cfg, const char *category, const char *variable, struct ast_str **field, const char *def)
+{
+    struct unload_string *us;
+    const char *tmp;
+
+    if (!(us = ast_calloc(1, sizeof(*us))))
+    {
+        return -1;
+    }
+
+    if (!(*field = ast_str_create(16))) {
+        ast_free(us);
+        return -1;
+    }
+
+    tmp = ast_variable_retrieve(cfg, category, variable);
+
+    ast_str_set(field, 0, "%s", tmp ? tmp : def);
+
+    us->str = *field;
+
+    AST_LIST_LOCK(&unload_strings);
+    AST_LIST_INSERT_HEAD(&unload_strings, us, entry);
+    AST_LIST_UNLOCK(&unload_strings);
+
+    return 0;
+}
+
+/**
+ * Convert string type message to json type.
+ * @param msg
+ * @return Success:json_t*, Fail:NULL
+ */
+static json_t* recv_parse(char* msg)
+{
+    json_t* j_out;
+    json_error_t error;
+
+    j_out = json_loads(msg, 0, &error);
+    if(j_out == NULL)
+    {
+        DEBUG("Could not convert json. msg[%s]\n", msg);
+        return NULL;
+    }
+    return j_out;
+}
+
+/**
+ * Main thread function.
+ *
+ * Binds to the connection_string and waits for new messages.
+ */
+static void zmq_cmd_thread(void)
+{
+    char buffer[MAX_RCV_BUF_LEN];
+    zmq_data_t* zmq_data;
+    int size;
+
+    while (1)
+    {
+        memset(buffer, 0x00, sizeof(buffer));
+        size = zmq_recv(g_app.sock_cmd, buffer, MAX_RCV_BUF_LEN - 1, 0);
+        if(size == -1)
+        {
+            continue;
+        }
+        else if(size > (MAX_RCV_BUF_LEN -1))
+        {
+            size = MAX_RCV_BUF_LEN - 1;
+        }
+        buffer[size] = 0;
+
+        zmq_data = calloc(1, sizeof(zmq_data_t));
+        zmq_data->zmq_sock = g_app.addr_cmd;
+
+        zmq_data->j_recv = recv_parse(buffer);
+        if(zmq_data->j_recv == NULL)
+        {
+            ERROR("Could not parse msg. msg[%s]\n", buffer);
+            zmq_send(zmq_data->zmq_sock, "Could not parse msg.", strlen("Could not parse msg."), 0);
+            continue;
+        }
+        zmq_cmd_handler(zmq_data);
+    }
+}
+
+static struct manager_custom_hook test_hook = {
+    .file = __FILE__,
+    .helper = &amihook_helper,
+};
+
+/*
+ * CLI command handler.
+ *
+ * Shows whether the socket is binded or not and the number of calls made so far.
+ */
+static char* handle_cli_zmq_manager_status(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a)
+{
+    switch (cmd) {
+        case CLI_INIT:
+        {
+            e->command = "zmq manager status";
+            e->usage =
+                    "Usage: zmq manager status\n"
+                    "       Shows useful stats about zmq manager usage\n";
+            return NULL;
+        }
+        break;
+
+        case CLI_GENERATE:
+        {
+            return NULL;
+        }
+        break;
+    }
+
+    if (a->argc != 3)
+    {
+        return CLI_SHOWUSAGE;
+    }
+
+    ast_cli(a->fd, "[cmd address: %s]\n", ast_str_buffer(g_app.addr_cmd));
+    ast_cli(a->fd, "[evt address: %s]\n", ast_str_buffer(g_app.addr_evt));
+
+
+    return CLI_SUCCESS;
+}
+
+
+static int unload_module(void)
+{
+    ast_manager_unregister_hook(&test_hook);
+    return ast_cli_unregister_multiple(cli_zmq_manager_evt, ARRAY_LEN(cli_zmq_manager_evt));
+}
+
+/**
+ *
+ * @return
+ */
+static int _load_module(void)
+{
+    struct ast_config *cfg;
+    struct ast_flags config_flags = {0};
+    int ret;
+    void*  context;
+
+    DEBUG("%s\n", "Loading zmq manager Config");
+    cfg = ast_config_load(g_app.config_name, config_flags);
+    if ((cfg == NULL) || (cfg == CONFIG_STATUS_FILEINVALID))
+    {
+        ast_log(LOG_WARNING, "Unable to load config for zmq manager: %s\n", g_app.config_name);
+        return AST_MODULE_LOAD_FAILURE;
+    }
+    else if (cfg == CONFIG_STATUS_FILEUNCHANGED)
+    {
+        return AST_MODULE_LOAD_SUCCESS;
+    }
+
+    // cmd socket
+    ret  = load_config_string(cfg, "global", "addr_cmd", &g_app.addr_cmd, "tcp://*:967");
+    if(ret < 0)
+    {
+        DEBUG("%s\n", "Could not load connection_string");
+        return AST_MODULE_LOAD_FAILURE;
+    }
+    DEBUG("cmd address. addr[%s]\n", ast_str_buffer(g_app.addr_cmd));
+
+    // evt socket
+    ret  = load_config_string(cfg, "global", "addr_evt", &g_app.addr_evt, "tcp://*:968");
+    if(ret < 0)
+    {
+        DEBUG("%s\n", "Could not load connection_string");
+        return AST_MODULE_LOAD_FAILURE;
+    }
+    DEBUG("evt address. addr[%s]\n", ast_str_buffer(g_app.addr_evt));
+
+    ast_config_destroy(cfg);
+
+    context = zmq_ctx_new();
+
+    // Make cmd socket
+    g_app.sock_cmd = zmq_socket(context, ZMQ_REP);
+    if(g_app.sock_cmd == NULL)
+    {
+        ERROR("Couldn't created the new socket [%s]\n", strerror(errno));
+        zmq_close (g_app.sock_cmd);
+        zmq_term (context);
+        return false;
+    }
+
+    ret = zmq_bind(g_app.sock_cmd, ast_str_buffer(g_app.addr_cmd));
+    if(ret == -1)
+    {
+        ERROR("Couldn't bind [%s]\n", strerror(errno));
+        zmq_close (g_app.sock_cmd);
+        zmq_term (context);
+        return false;
+    }
+
+    // Make evt socket
+    g_app.sock_evt = zmq_socket(context, ZMQ_PUB);
+    if(g_app.sock_evt == NULL)
+    {
+        ERROR("Couldn't created the evt socket [%s]\n", strerror(errno));
+        zmq_close (g_app.sock_evt);
+        zmq_term (context);
+        return false;
+    }
+
+    ret = zmq_bind(g_app.sock_evt, ast_str_buffer(g_app.addr_evt));
+    if(ret == -1)
+    {
+        ERROR("Couldn't bind [%s]\n", strerror(errno));
+        zmq_close (g_app.sock_evt);
+        zmq_term (context);
+        return false;
+    }
+
+    DEBUG("%s\n", "About to call ast_zmq_start");
+    ret = ast_zmq_start();
+
+    return AST_MODULE_LOAD_SUCCESS;
+
+}
+
+static int load_module(void)
+{
+    int ret;
+
+    ret = asprintf(&g_app.config_name, "zmq_manager.conf");
+
+    ret = _load_module();
+    if(ret != AST_MODULE_LOAD_SUCCESS)
+    {
+        ERROR("Could not load module! ret[%d]\n", ret);
+        return AST_MODULE_LOAD_FAILURE;
+    }
+
+    DEBUG("%s\n", "Load correctly..");
+    ret = ast_cli_register_multiple(cli_zmq_manager_evt, ARRAY_LEN(cli_zmq_manager_evt));
+
+    return AST_MODULE_LOAD_SUCCESS;
+//    return ret ? AST_MODULE_LOAD_DECLINE : AST_MODULE_LOAD_SUCCESS;
+}
+
+/**
+ * zmq command msg handler
+ * @param data
+ */
+static void zmq_cmd_handler(void *data)
+{
+    int ret;
+    json_t* j_tmp;
+    zmq_data_t* zmq_data;
+    char* dumps;
+    struct manager_custom_hook* hook;
+    const char *key;
+    json_t *value;
+    char* str_resp;
+    char str_cmd[10000];
+
+    DEBUG("ast_worker!! data[%p]\n", data);
+
+    zmq_data = (zmq_data_t*)data;
+
+    dumps = json_dumps(zmq_data->j_recv, JSON_INDENT(1));
+    DEBUG("ast_start_worker. msg[%s]\n", dumps);
+    free(dumps);
+
+    hook = calloc(1, sizeof(struct manager_custom_hook));
+    hook->file = __FILE__;
+    hook->helper = &zmq_cmd_helper;
+
+    memset(str_cmd, 0x00, sizeof(str_cmd));
+
+    // Get action
+    j_tmp = json_object_get(zmq_data->j_recv, "action");
+    if(j_tmp == NULL)
+    {
+        ERROR("%s\n", "Could not get the action.");
+    }
+    sprintf(str_cmd, "action:%s\n", json_string_value(j_tmp));
+
+    json_object_foreach(zmq_data->j_recv, key, value)
+    {
+        ret = strcmp(key, "action");
+        if(ret == 0)
+        {
+            continue;
+        }
+        sprintf(str_cmd, "%s%s:%s\n", str_cmd, key, json_string_value(value));
+    }
+    sprintf(str_cmd, "%s\n", str_cmd);
+
+    DEBUG("action command. command[%s]\n", str_cmd);
+
+    g_json_res = json_object();
+    ret = ast_hook_send_action(hook, str_cmd);
+    if(ret != 0)
+    {
+        free(hook);
+        return;
+    }
+
+    str_resp = json_dumps(g_json_res, JSON_INDENT(1));
+    json_decref(g_json_res);
+    if(str_resp == NULL)
+    {
+        return;
+    }
+
+    ret = zmq_send(zmq_data->zmq_sock, str_resp, strlen(str_resp), 0);
+    DEBUG("set_hook_send_action. ret[%d], msg[%s]\n", ret, str_resp);
+
+    free(hook);
+    free(str_resp);
+}
+
+/**
+ * Main thread starter.
+ *
+ * Starts the main thread.
+ */
+static int ast_zmq_start(void)
+{
+    int ret;
+
+    // cmd sock
+    DEBUG("%s\n", "start zmq cmd thread");
+    ret = ast_pthread_create_background(&g_app.pth_cmd, NULL, (void*)&zmq_cmd_thread, NULL);
+    if(ret > 0)
+    {
+        ERROR("Unable to launch thread for action cmd. err[%s]\n", strerror(errno));
+        return false;
+    }
+
+    // evt sock
+    DEBUG("%s\n", "start zmq evt thread");
+    struct manager_custom_hook* hook;
+    hook = calloc(1, sizeof(struct manager_custom_hook));
+    hook->file = __FILE__;
+    hook->helper = &zmq_evt_helper;
+    DEBUG("%s\n", "Before hook");
+    ast_manager_register_hook(hook);
+    DEBUG("%s\n", "After hook");
+
+    return true;
+}
+
+static void trim(char * s)
+{
+    char * p = s;
+    int l = strlen(p);
+
+    while(isspace(p[l - 1])) p[--l] = 0;
+    while(* p && isspace(* p)) ++p, --l;
+
+    memmove(s, p, l + 1);
+}
+
+
+static int zmq_evt_helper(int category, const char *event, char *content)
+{
+
+    json_t* j_out;
+    json_t* j_tmp;
+    int i;
+    int ret;
+    int cnt;
+    char* line;
+    char* key;
+    char* value;
+    char** parse;
+    char*  buf_send;
+
+    DEBUG("zmq_evt_handler. category[%d], event[%s], content[%s]\n", category, event, content);
+
+    parse = calloc(1024, sizeof(char*));
+
+    line = strtok(content, "\n");
+    parse[0] = strdup(line);
+
+    cnt = 1;
+    while(true)
+    {
+        line = strtok(NULL, "\n");
+        if(line == NULL)
+        {
+            break;
+        }
+        ret = strlen(line);
+        if(ret < 2)
+        {
+            break;
+        }
+
+        parse[cnt] = strdup(line);
+        cnt++;
+    }
+
+    // parse
+    DEBUG("check. cnt[%d]\n", cnt);
+    j_out = json_object();
+    for(i = 0; i < cnt; i++)
+    {
+        key = strtok(parse[i], ":");
+        value = strtok(NULL, ":");
+        trim(key);
+        trim(value);
+        j_tmp = json_string(value);
+        json_object_set(j_out, key, j_tmp);
+        json_decref(j_tmp);
+
+        DEBUG("parse[%s], key[%s], value[%s]\n", parse[i], key, value);
+    }
+
+    buf_send = json_dumps(j_out, JSON_INDENT(0));
+    ret = zmq_send(g_app.sock_evt,  buf_send, strlen(buf_send), 0);
+    DEBUG("send buf. ret[%d], buf[%s]\n", ret, buf_send);
+
+    // free
+    for(i = 0; i < 1024; i++)
+    {
+        if(parse[i] == NULL)
+        {
+            break;
+        }
+        free(parse[i]);
+    }
+    free(parse);
+    json_decref(j_out);
+    free( buf_send);
+
+    return 0;
+
+
+//    key = strtok(content, ":");
+//    value = strtok(NULL, "\r");
+//
+//    json_object_set(g_json_evt, key, json_string(value));
+//
+//    tmp = strtok(NULL, "\r");
+//    if(tmp == value)
+//    {
+//        return;
+//    }
+//
+//    // send to client
+//    buf = json_dumps(g_json_evt, JSON_INDENT(1));
+//    zmq_send(g_app.sock_evt, buf, strlen(buf), 0);
+//    free(buf);
+//    json_decref(g_json_evt);
+
+    return true;
+}
+
+
+AST_MODULE_INFO_STANDARD(ASTERISK_GPL_KEY, "ZMQ Manager Module");
